be5a1b03eee38b5d45694abc6492131407d9c3c3
alibokiev <sodley101401@gmail.com>
2023-08-12 16:31:56 +0500

    initial: create symfony app

---
+++ tests/Functional/Controller/CheckPriceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Functional\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CheckPriceTest extends WebTestCase
+{
+
+}
