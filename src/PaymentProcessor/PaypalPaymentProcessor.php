8bda68ee683fd7c92dbc75fffc5aafccf06b550e
ali <bokievalinaziri@gmail.com>
2024-04-27 13:15:13 +0300

    fix: payment processors

--- src/PaymentProcessor/PaypalPaymentProcessor.php
+++ src/PaymentProcessor/PaypalPaymentProcessor.php
@@ -1,16 +1,24 @@
 <?php
 
-class PaypalPaymentProcessor
+namespace App\PaymentProcessor;
+
+use Contracts\PaymentProcessorInterface;
+use Exception;
+
+class PaypalPaymentProcessor implements PaymentProcessorInterface
 {
     /**
+     * @param int $price
+     * @return bool true if payment was succeeded, false otherwise
      * @throws Exception in case of a failed payment
      */
-    public function pay(int $price): void
+    public function pay(int $price): bool
     {
         if ($price > 100) {
             throw new Exception('Too high price');
         }
 
         //process payment logic
+        return true;
     }
 }
--- src/PaymentProcessor/StripePaymentProcessor.php
+++ src/PaymentProcessor/StripePaymentProcessor.php
@@ -1,14 +1,21 @@
 <?php
 
-class StripePaymentProcessor
+namespace App\PaymentProcessor;
+
+use Contracts\PaymentProcessorInterface;
+use Exception;
+
+class StripePaymentProcessor implements PaymentProcessorInterface
 {
     /**
+     * @param int $price
      * @return bool true if payment was succeeded, false otherwise
+     * @throws Exception
      */
     public function processPayment(int $price): bool
     {
         if ($price < 10) {
-            return false;
+            throw new Exception('Too high price');
         }
 
         //process payment logic
